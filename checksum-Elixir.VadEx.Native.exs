32c163466164342150d61e07e76052262f2746a6
CircuitSerein <263436322+CircuitSerein@users.noreply.github.com>
2026-06-24 18:34:54 +0300

    chore(release): add precompiled NIF checksums for v0.1.0

---
+++ checksum-Elixir.VadEx.Native.exs
@@ -0,0 +1,6 @@
+%{
+  "libvad_ex-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:7532e0c8dd4763c2f6517e9252f9dd167e80042f5dd5512d26b37270fdbd87bc",
+  "libvad_ex-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:726218136dc83738996147b786838d23ea2fa849bdf09f46450a115b6d8dd404",
+  "libvad_ex-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:0a409389c2631fbc3e5723d523aa84b4be4efe8eee98a73c81587d01c860b0b3",
+  "vad_ex-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:389e37c195c5dfac73548708bcdcaa302e40716cc3d16104109241e1708a2b58",
+}
